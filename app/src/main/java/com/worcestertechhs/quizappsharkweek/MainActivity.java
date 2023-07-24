747f592d0ef12086211826912d8c58be252d141c
Mary C. Miller <MCMiller@worcesterk12.org>
2023-07-24 15:39:03 -0400

    Added first question to T/F quiz and Toast for Correct/Wrong

--- app/src/main/java/com/worcestertechhs/quizappsharkweek/MainActivity.java
+++ app/src/main/java/com/worcestertechhs/quizappsharkweek/MainActivity.java
@@ -3,12 +3,43 @@ package com.worcestertechhs.quizappsharkweek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView questionTV;
+    Button falseBTN,trueBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        questionTV = (TextView) findViewById(R.id.questionTV);
+        falseBTN = (Button) findViewById(R.id.falseBTN);
+        trueBTN = (Button) findViewById(R.id.trueBTN);
+
+        falseBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence message = "You are NOT correct!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(getApplicationContext(), message, duration);
+                toast.show();
+            }
+        });
+        trueBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence message = "You are Correct!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(getApplicationContext(), message, duration);
+                toast.show();
+            }
+        });
     }
 }
