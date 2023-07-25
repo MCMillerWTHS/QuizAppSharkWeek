a24199a0f0788f9611fa529791b380088d2179c5
Mary C. Miller <MCMiller@worcesterk12.org>
2023-07-25 10:26:55 -0400

    Added Activity for Score to change from one screen to another.  Display score on seperate screen.

--- app/src/main/java/com/worcestertechhs/quizappsharkweek/MainActivity.java
+++ app/src/main/java/com/worcestertechhs/quizappsharkweek/MainActivity.java
@@ -2,6 +2,8 @@ package com.worcestertechhs.quizappsharkweek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +13,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView questionTV;
-    Button falseBTN,trueBTN;
-    int count = 0;
+    Button falseBTN, trueBTN, scoreBTN;
+    int score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,27 +23,39 @@ public class MainActivity extends AppCompatActivity {
         questionTV = (TextView) findViewById(R.id.questionTV);
         falseBTN = (Button) findViewById(R.id.falseBTN);
         trueBTN = (Button) findViewById(R.id.trueBTN);
+        scoreBTN = (Button) findViewById(R.id.scoreBTN);
+        score = 0;
 
         falseBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count = count - 1;
-                CharSequence message = "You are NOT correct! Score: " + count;
+                score--;
+                CharSequence message = "You are NOT correct!";
                 int duration = Toast.LENGTH_SHORT;
+                Context myContext = getApplicationContext();  //Shows a different way to set up Context.
 
-                Toast toast = Toast.makeText(getApplicationContext(), message, duration);
+                Toast toast = Toast.makeText(myContext, message, duration);
                 toast.show();
             }
         });
         trueBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count = count + 1;
-                CharSequence message = "You are Correct! Score: " + count;
-                int duration = Toast.LENGTH_LONG;
+                score++;
+                CharSequence message = "You are Correct!";
+                int duration = Toast.LENGTH_SHORT;
 
+                //You can use getApplicationContext() call as a parameter.
                 Toast toast = Toast.makeText(getApplicationContext(), message, duration);
                 toast.show();
+            }
+        });
+        scoreBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent scoreIntent = new Intent(MainActivity.this, ScoreActivity.class);
+                    scoreIntent.putExtra("score", score);
+                    startActivity(scoreIntent);
             }
         });
     }
